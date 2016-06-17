a5f6d6420a355706bafa75dbbfe2e2b992b8ee02
mdoederlein <michael.doederlein@outlook.com>
2016-06-17 14:19:32 +0200

    .

---
+++ odoo_v9_install/recipes/odoo.rb
@@ -0,0 +1,59 @@
+#
+# Cookbook Name:: install_odoo
+# Recipe:: odoo
+#
+# Copyright (c) 2016 Michael Doederlein, All Rights Reserved.
+#
+git "#{node['install_odoo']['homedir']}/odoo-server" do
+  user node['install_odoo']['user']
+  group node['install_odoo']['group']
+  depth 1
+  repository node['install_odoo']['git_odoo_repository']
+  revision node['install_odoo']['git_odoo_branch']
+  action :sync
+end
+
+# make directory for custom addons
+execute 'mkdir-custom-add' do
+  user node['install_odoo']['user']
+  group node['install_odoo']['group']
+  command "mkdir -p #{node['install_odoo']['custom_addons']}"
+  not_if { File.exist?("#{node['install_odoo']['custom_addons']}") }
+end
+
+# install further requirements
+execute 'pip-install' do
+  command 'pip install -r /opt/odoo/odoo-server/requirements.txt'
+  not_if 'pip list | awk \'{print $1}\' | grep xlwt'
+end
+#execute 'easy_install' do
+# scheinbar ist das Verzeichnis pyPdf vorhanden, auch wenn easy_install nicht gelaufen ist -> prüfen!
+#  command 'easy_install pyPdf vatnumber pydot psycogreen suds ofxparse'
+#  not_if { File.exist?("/usr/local/lib/python2.7/dist-packages/pyPdf") }
+#end
+
+# add log-path
+directory '/var/log/odoo' do
+  owner node['install_odoo']['user']
+  group 'root'
+end
+
+# create openerp-server.conf
+template '/etc/odoo-server.conf' do
+  source 'odoo-server.conf.erb'
+  owner node['install_odoo']['user']
+  group 'root'
+  mode '0640'
+end
+
+# create start-/stop script
+template '/etc/init.d/odoo-server' do
+  source 'odoo-server.erb'
+  mode '0755'
+end
+
+# bring service alive
+service 'odoo-server' do
+  action [:enable, :start]
+  supports :status => true, :start => true, :stop => true, :restart => true
+end
---
+++ odoo_v9_install/recipes/postgresql.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: install_odoo
+# Recipe:: postgresql
+#
+# Copyright (c) 2016 Michael Doederlein, All Rights Reserved.
+
+# Install postgresql client and server
+include_recipe 'postgresql::server'
+include_recipe 'postgresql::client'
+
+# add database user
+execute 'create dbuser' do
+  user "postgres"
+  command "createuser -s odoo"
+  not_if 'psql -c \'\du\'| awk \'{print $1}\' | grep odoo'
+end
+
+# restart postgresql service
+service 'postgresql' do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :restart ]
+end
