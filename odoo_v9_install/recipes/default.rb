7a76fcc4a3d39544648d74171bcf79b29385f011
mdoederlein <michael.doederlein@outlook.com>
2016-07-05 15:22:11 +0200

    xxx

--- odoo_v9_install/recipes/default.rb
+++ odoo_v9_install/recipes/default.rb
@@ -3,4 +3,4 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-Chef::Log.info("********** Hello, World! **********")
+Chef::Log.info("********** Preparing for odoo **********")
