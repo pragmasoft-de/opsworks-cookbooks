309b63ac363fe03d1020f3f9dc51e8f1002f12ae
mdoederlein <michael.doederlein@outlook.com>
2016-07-22 18:35:21 +0200

    generate_ssl_certs

---
+++ letsencrypt/recipes/generate_ssl_certs.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: letsencrypt
+# Recipe:: generate_ssl_certs
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Include the recipe to install the gems
+include_recipe 'letsencrypt'
+
+# Set up contact information. Note the mailto: notation
+node.set['letsencrypt']['contact'] = [ 'mailto:me@example.com' ] 
+# Real certificates please...
+node.set['letsencrypt']['endpoint'] = 'https://acme-v01.api.letsencrypt.org' 
+
+site="cloud-logic.de"
+sans=Array[ "www.#{site}" ]
+
+# Set up your server here...
+
+# Let's letsencrypt
+
+# Generate a self-signed if we don't have a cert to prevent bootstrap problems
+letsencrypt_selfsigned "#{site}" do
+    crt     "/etc/apache2/ssl/#{site}.crt"
+    key     "/etc/apache2/ssl/#{site}.key"
+    chain    "/etc/apache2/ssl/#{site}.pem"
+    owner   "apache"
+    group   "apache"
+    notifies :restart, "service[apache2]", :immediate
+    not_if do
+        # Only generate a self-signed cert if needed
+        ::File.exists?("/etc/apache2/ssl/#{site}.crt")
+    end
+end
+
+# Get and auto-renew the certificate from letsencrypt
+letsencrypt_certificate "#{site}" do
+    crt      "/etc/apache/ssl/#{site}.crt"
+    key      "/etc/apache/ssl/#{site}.key"
+    chain    "/etc/apache/ssl/#{site}.pem"
+    method   "http"
+    wwwroot  "/var/www/#{site}/htdocs/"
+    notifies :restart, "service[apache2]"
+    alt_names sans
+end
