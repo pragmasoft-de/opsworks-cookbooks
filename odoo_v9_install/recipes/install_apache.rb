b48b78c673990bffdfa4c11076e6631c819d8c65
mdoederlein <michael.doederlein@outlook.com>
2016-08-05 15:29:00 +0200

    added apache install recipe

---
+++ odoo_v9_install/recipes/install_apache.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: odoo_v9_install
+# Recipe:: install_apache
+#
+# Copyright (c) 2016 Michael Doederlein, All Rights Reserved.
+#
+include_recipe 'apache2'
+include_recipe 'apache2::mod_proxy_http'
+include_recipe 'apache2::mod_headers'
+include_recipe 'apache2::mod_rewrite'
+include_recipe 'apache2::mod_ssl'
