8ebb45409941ea0e36c5e4e8de9707b05c4573d2
mdoederlein <michael.doederlein@outlook.com>
2016-08-09 18:32:09 +0200

    .

---
+++ dns_update/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: dns_update
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
---
+++ dns_update/recipes/update_route53.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: dns_update
+#
+# Copyright (c) 2016 Michael Doederlein, All Rights Reserved.
+require 'net/http'
+include_recipe 'route53'
+
+route53_record "create a record" do
+  name  node[:opsworks][:instance][:hostname] + '.cloud-logic.de'
+  value Net::HTTP.get(URI.parse('http://169.254.169.254/latest/meta-data/public-ipv4'))
+  type  "A"
+  ttl   60
+  zone_id               node[:dns_zone_id]
+  aws_access_key_id     node[:custom_access_key]
+  aws_secret_access_key node[:custom_secret_key]
+  overwrite true
+  action :create
+end
