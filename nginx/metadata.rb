a50a4ce25614cf221ded251a91b7d3e4e008c2b8
mdoederlein <michael.doederlein@outlook.com>
2016-08-22 18:48:35 +0200

    .

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -9,11 +9,11 @@ recipe 'nginx',         'Installs nginx package and sets up configuration with D
 recipe 'nginx::source', 'Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available'
 
 depends 'apt',             '>= 2.2'
-depends 'bluepill',        '~> 2.3'
-depends 'build-essential', '~> 2.0'
-depends 'ohai',            '~> 2.0'
-depends 'runit',           '~> 1.2'
-depends 'yum-epel',        '~> 0.3'
+depends 'bluepill',        '>= 2.3'
+depends 'build-essential', '>= 2.0'
+depends 'ohai',            '>= 2.0'
+depends 'runit',           '>= 1.2'
+depends 'yum-epel',        '>= 0.3'
 
 supports 'amazon'
 supports 'centos'
