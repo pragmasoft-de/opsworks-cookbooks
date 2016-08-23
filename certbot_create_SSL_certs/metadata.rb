c56f531b8864a2a4fc5ece21c1bdd24505ae360d
mdoederlein <michael.doederlein@outlook.com>
2016-08-23 12:38:54 +0200

    .

--- certbot_create_SSL_certs/metadata.rb
+++ certbot_create_SSL_certs/metadata.rb
@@ -1,7 +1,9 @@
 name 'certbot_create_SSL_certs'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Michael Doederlein'
+maintainer_email 'michael.doederlein@cloud-logic.net'
 license 'all_rights'
 description 'Installs/Configures certbot_create_SSL_certs'
 long_description 'Installs/Configures certbot_create_SSL_certs'
 version '0.1.0'
+
+depends 'certbot', '>= 0.1.0'
--- certbot_create_SSL_certs/recipes/default.rb
+++ certbot_create_SSL_certs/recipes/default.rb
@@ -4,8 +4,6 @@
 #
 # Copyright (c) 2016 Michael Doederlein, All Rights Reserved.
 
-depends 'certbot', '>= 0.1.0'
-
 certbot_certonly_webroot 'create_SSL_certs' do
    webroot_path '/var/www/certbot'
    email 'michael.doederlein@cloud-logic.net'
